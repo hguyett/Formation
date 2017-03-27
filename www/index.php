8affc4d356570be12102474dda6f7019c2d82254
Hadrien Guyette <hadrien.guyette@bluesustainia.be>
2017-03-27 18:20:24 +0200

    Notes - PHP OO - Partie 2

--- www/index.php
+++ www/index.php
@@ -1,12 +1,27 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>News bulletin</title>
-</head>
-<body>
-<?php include 'Controller.php'; ?>
-</body>
-</html>
+<?php
+class Nombre
+{
+  private $_nbr;
+
+  public function __construct($nbr)
+  {
+    $this->_nbr = $nbr;
+  }
+
+  public function getNbr()
+  {
+    return $this->_nbr;
+  }
+}
+
+$closure = function() {
+  var_dump($this->_nbr + 5);
+};
+
+$two = new Nombre(2);
+$three = new Nombre(3);
+
+$test = $closure->call($two);
+$test2 = $closure->call($three);
+echo $test;
+echo $two->getNbr();
